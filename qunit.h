0e03b298227391f9f98781a37780c40097869ff6
Alfredo Mungo <alfredo.mungo@openmailbox.org>
2015-03-03 23:07:55 +0100

    Added documentation Fixed issue which prevented the test function from immediately returning when using the pass/fail macro Improved the header function

--- qunit.h
+++ qunit.h
@@ -24,36 +24,92 @@
 
 #define QUNIT_VERSION_MAJOR 0
 #define QUNIT_VERSION_MINOR 1
-#define QUNIT_VERSION_PATCH 0
+#define QUNIT_VERSION_PATCH 1
 #define __QUNIT_VERSION(__maj, __min, __pat) #__maj "." #__min "." #__pat
 #define _QUNIT_VERSION(__maj, __min, __pat) __QUNIT_VERSION(__maj, __min, __pat)
 #define QUNIT_VERSION _QUNIT_VERSION(QUNIT_VERSION_MAJOR, QUNIT_VERSION_MINOR, QUNIT_VERSION_PATCH)
 
+/* Maximum number of tests per test case. Can be overridden in test files. */
 #ifndef QUNIT_MAX_TESTS
   #define QUNIT_MAX_TESTS 64
 #endif
 
+/* Maximum number of reporters per test case. Can be overridden in test files. */
 #ifndef QUNIT_MAX_REPORTERS
   #define QUNIT_MAX_REPORTERS 4
 #endif
 
+/*
+ * Test declaration macro.
+ *
+ * Substitutes to a test function declaration.
+ *
+ * __test_name: The name to assign to the test
+ *
+ * Example:
+ *  TEST(myTest1) {
+ *    ...
+ *  }
+ */
 #define QUNIT_TEST(__test_name) void __test_name(QUNIT_RESULT *__qunit_result)
 
+/*
+ * Test registration macro.
+ * 
+ * Registers a test against a test case.
+ *
+ * __tcase: Pointer to the target test case
+ * __test_func: Test function name
+ *
+ * Example:
+ *  qunit_tcase_add(&tcase, myTest1);
+ */
 #define qunit_tcase_add(__tcase, __test_func) __qunit_tcase_add(__tcase, #__test_func, __test_func)
 
-#define qunit_pass() *__qunit_result = QUNIT_RESULT_PASSED
-#define qunit_fail() *__qunit_result = QUNIT_RESULT_FAILED
+/*
+ * Test pass macro.
+ *
+ * Immediately passes the current test.
+ */
+#define qunit_pass() *__qunit_result = QUNIT_RESULT_PASSED; return
 
+/*
+ * Test fail macro.
+ *
+ * Immediately fails the test.
+ */
+#define qunit_fail() *__qunit_result = QUNIT_RESULT_FAILED; return
+
+/*
+ * Condition assert macro.
+ *
+ * Fails the test if the given condition evaluates to 0.
+ *
+ * __code: Code representing a condition to evaluate
+ *
+ * Example:
+ *  qunit_assert(1 == 5)
+ */
 #define qunit_assert(__code) if(!__code) qunit_fail()
 
+/*
+ * Test result enumeration.
+ */
 typedef enum _QUNIT_RESULT {
-  QUNIT_RESULT_PASSED = 0,
-  QUNIT_RESULT_FAILED = 1
+  QUNIT_RESULT_PASSED = 0, /* Test passed */
+  QUNIT_RESULT_FAILED = 1 /* Test failed */
 } QUNIT_RESULT;
 
-typedef void (*QUNIT_TESTFUNC)(QUNIT_RESULT*);
-typedef void (*QUNIT_REPORTERFUNC)(const char *, QUNIT_RESULT);
+typedef void (*QUNIT_TESTFUNC)(QUNIT_RESULT*); /* Test function pointer */
+typedef void (*QUNIT_REPORTERFUNC)(const char *, QUNIT_RESULT); /* Reporter function pointer */
 
+/*
+ * Test case structure.
+ *
+ * A test is a set of tests and reporters.
+ *
+ * The structure contains the following data about each test: function pointer, string name, result.
+ */
 typedef struct _QUNIT_TESTCASE {
   const char *names[QUNIT_MAX_TESTS];
   QUNIT_TESTFUNC tests[QUNIT_MAX_TESTS];
@@ -61,11 +117,28 @@ typedef struct _QUNIT_TESTCASE {
   QUNIT_REPORTERFUNC reporters[QUNIT_MAX_REPORTERS];
 } QUNIT_TESTCASE;
 
+/*
+ * Initializes a test case.
+ *
+ * tcase: Pointer to the test case to initialize
+ */
 void qunit_tcase_init(QUNIT_TESTCASE *tcase) {
   memset(tcase->tests, 0, sizeof(QUNIT_TESTFUNC) * QUNIT_MAX_TESTS);
   memset(tcase->reporters, 0, sizeof(QUNIT_REPORTERFUNC) * QUNIT_MAX_REPORTERS);
 }
 
+/*
+ * Test case registration function.
+ *
+ * This function registers a test against a test case.
+ *
+ * tcase: The target test case
+ * test_name: The test name
+ * test_func: The test function pointer
+ *
+ * RETURN: 1 on success, 0 on failure
+ * SEE: qunit_tcase_add()
+ */
 int __qunit_tcase_add(QUNIT_TESTCASE *tcase, const char* test_name, QUNIT_TESTFUNC test_func) {
   size_t i;
 
@@ -83,6 +156,14 @@ int __qunit_tcase_add(QUNIT_TESTCASE *tcase, const char* test_name, QUNIT_TESTFU
   }
 }
 
+/*
+ * Adds a reporter to a test case.
+ *
+ * tcase: Target test case pointer
+ * func: Reporter function pointer
+ *
+ * RETURN: 1 on success, 0 on failure
+ */
 int qunit_tcase_add_reporter(QUNIT_TESTCASE *tcase, QUNIT_REPORTERFUNC func) {
   size_t i;
 
@@ -97,10 +178,30 @@ int qunit_tcase_add_reporter(QUNIT_TESTCASE *tcase, QUNIT_REPORTERFUNC func) {
   }
 }
 
+/*
+ * Prints the QUnit program header.
+ *
+ * You can add the test version to the header by defining the TESTFILE_VERSION preprocessor constant.
+ */
 void qunit_print_header() {
-  puts("QUnit v" QUNIT_VERSION "\n");
+  puts("Test file    : " __BASE_FILE__);
+  #ifdef TESTFILE_VERSION
+  puts("Test version : " TESTFILE_VERSION);
+  #endif /* TESTFILE_VERSION */
+  puts("QUnit version: " QUNIT_VERSION);
+  puts("Compiled on  : " __DATE__);
+  puts("");
 }
 
+/*
+ * Runs a test case.
+ *
+ * Runs every test of the test case and after each test the reporter function is called to report the results.
+ *
+ * tcase: Pointer to the test case to run
+ *
+ * RETURN: The number of failed tests
+ */
 unsigned int qunit_tcase_run(QUNIT_TESTCASE *tcase) {
   size_t i, j;
   unsigned int failed = 0;
@@ -122,6 +223,14 @@ unsigned int qunit_tcase_run(QUNIT_TESTCASE *tcase) {
   return failed;
 }
 
+/*
+ * Console reporter function.
+ *
+ * This is a reporter function that outputs the test results to the console.
+ *
+ * name: The test name
+ * result: The test result
+ */
 void qunit_reporter_console(const char* name, QUNIT_RESULT result) {
   const char* s_result = (result == QUNIT_RESULT_PASSED? "PASSED": "FAILED");
 
