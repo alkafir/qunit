34f84f9e62669fe88324bdbaa5f5a3e103d2b6c9
Alfredo Mungo <alfredo.mungo@openmailbox.org>
2015-03-11 15:43:50 +0100

    Updated samples

--- samples/vectors.c
+++ samples/vectors.c
@@ -8,24 +8,10 @@ QUNIT_TEST(vector_equals) {
 }
 
 QUNIT_TEST(vector_equals2) {
-  struct s{ int a; char b; };
-  struct s a[] = {{1, 2}, {1, 3}};
-  struct s b[] = {{1, 2}, {1, 3}};
+  unsigned int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  unsigned int b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-  qunit_assert_vector_equals2(a, b, 2, sizeof(struct s));
-}
-
-QUNIT_TEST(vector_equals3) {
-  struct s{ int a; char b; };
-  typedef struct s* s_ptr;
-
-  struct s s1 = {1, 2};
-  struct s s2 = {1, 3};
-
-  s_ptr a[] = {&s1, &s2};
-  s_ptr b[] = {&s1, &s2};
-
-  qunit_assert_vector_equals3(a, b, 2, sizeof(struct s));
+  qunit_assert_vector_equals2(a, b, 10, sizeof(unsigned int));
 }
 
 int main(int argc, char **argv) {
@@ -34,7 +20,6 @@ int main(int argc, char **argv) {
   qunit_tcase_init(&tcase);
   qunit_tcase_add(&tcase, vector_equals);
   qunit_tcase_add(&tcase, vector_equals2);
-  qunit_tcase_add(&tcase, vector_equals3);
 
   qunit_print_header();
   return (int)qunit_tcase_run(&tcase);
